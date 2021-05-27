62278747663ce4a3a5a9aa2af638cc979c3f128e
Abulkhair23 <78597371+Abulkhair23@users.noreply.github.com>
2021-05-28 02:59:55 +0600

    Documentation

--- Intranet/src/Database.java
+++ Intranet/src/Database.java
@@ -4,21 +4,40 @@ import java.util.HashMap;
 import java.util.Base64;
 import java.util.Collections;
 /**
-* @generated
-*/
+ * Database is a storage of all objects: users, courses, etc.
+ * Only exists in one copy.
+ */
 public class Database implements Serializable {
     
     /**
-    * @generated
+    * ArrayList which contains all users.
     */
     protected static ArrayList<User> users = new ArrayList<>();
+    /**
+     * ArrayList which contains all courses.
+     */
     protected static ArrayList<Course> courses = new ArrayList<>();
+//    /**
+//     * ArrayList which contains all logs.
+//     */
     // protected static ArrayList<LogFile> logs = new ArrayList<LogFile>();
+    /**
+     * HashMap which contains all marks of particular course.
+     */
     protected static HashMap<Course, Mark> marks = new HashMap<>();
+    /**
+     * ArrayList which contains all news.
+     */
     protected static ArrayList<News> news = new ArrayList<>();
+    /**
+     * ArrayList which contains all messages.
+     */
     protected static ArrayList<Message> messages = new ArrayList<>();
+    /**
+     * boolean value of status of registration.
+     */
     protected static boolean registrationIsOpen = true;
-
+    
     protected static String getKeyword() {
         return "Test";
     }
@@ -26,19 +45,22 @@ public class Database implements Serializable {
     private static Database INSTANCE = null;
 
     private Database() {}
-
+    /**
+     * Gets the Database object.
+     * @return Returns Database if it exists, otherwise return new Database object.
+     */
     public static Database getInstance() {
         if (INSTANCE == null) INSTANCE = new Database();
         return INSTANCE;
     }
-
+   
     public static void serUsers() throws IOException{
         FileOutputStream fos = new FileOutputStream("users.out");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(users);
         oos.close();
     }
-
+   
     @SuppressWarnings("unchecked")
     public  static void desUsers() throws IOException, ClassNotFoundException {
         FileInputStream fis = new FileInputStream("users.out");
@@ -50,7 +72,7 @@ public class Database implements Serializable {
 			System.out.println(u);
 		}
     }
-
+    
     public static void serCourses() throws IOException {
         FileOutputStream fos = new FileOutputStream("courses.out");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -66,7 +88,10 @@ public class Database implements Serializable {
         oin.close();
         fis.close();
     }
-
+    /**
+     * Get all teachers.
+     * @return Returns toString representation of teachers.
+     */
     public static String getTeachers() {
         StringBuilder s = new StringBuilder();
         for (User user: users) {
@@ -77,7 +102,10 @@ public class Database implements Serializable {
         }
         return s.toString();
     }
-
+    /**
+     * Get all students.
+     * @return Returns toString representation of students.
+     */
     public static String getStudents() {
         StringBuilder s = new StringBuilder();
         for (User user: users) {
@@ -88,7 +116,10 @@ public class Database implements Serializable {
         }
         return s.toString();
     }
-    
+    /**
+     * Get all students.
+     * @return Returns ArrayList of all students.
+     */
     public static ArrayList<Student> getStudentsList() {
     	ArrayList<Student> s = new ArrayList<Student>();
         for (User user: users) {
@@ -99,7 +130,10 @@ public class Database implements Serializable {
         }
         return s;
     }
-    
+    /**
+     * Get all students ordered by name.
+     * @return Returns ArrayList of all students ordered by name.
+     */
     public static ArrayList<Student> getStudentsListByName() {
     	ArrayList<Student> s = new ArrayList<Student>();
         for (User user: users) {
@@ -111,7 +145,10 @@ public class Database implements Serializable {
         Collections.sort(s, new SortStudentByName());
         return s;
     }
-
+    /**
+     * Get all employees.
+     * @return Returns toString representation of employees.
+     */
     public static String getEmployees() {
         StringBuilder s = new StringBuilder();
         for (User user: users) {
@@ -122,7 +159,10 @@ public class Database implements Serializable {
         }
         return s.toString();
     }
-
+    /**
+     * Get all managers.
+     * @return Returns toString representation of managers.
+     */
     public static String getManagers() {
         StringBuilder s = new StringBuilder();
         for (User user: users) {
@@ -133,7 +173,10 @@ public class Database implements Serializable {
         }
         return s.toString();
     }
-
+    /**
+     * Get all admins.
+     * @return Returns toString representation of admins.
+     */
     public static String getAdmins() {
         StringBuilder s = new StringBuilder();
         for (User user: users) {
@@ -144,7 +187,10 @@ public class Database implements Serializable {
         }
         return s.toString();
     }
-
+    /**
+     * Get all librarians.
+     * @return Returns toString representation of librarians.
+     */
     public static String getLibrarians() {
         StringBuilder s = new StringBuilder();
         for (User user: users) {
@@ -155,7 +201,10 @@ public class Database implements Serializable {
         }
         return s.toString();
     }
-
+    /**
+     * Get all courses.
+     * @return Returns toString representation of courses.
+     */
     public static String getCourses() {
         StringBuilder s = new StringBuilder();
         for (Course course: courses) {
@@ -165,11 +214,18 @@ public class Database implements Serializable {
         }
         return s.toString();
     }
-
+    /**
+     * Get all messages.
+     * @return Returns ArrayList of all messages.
+     */
     public static ArrayList<Message> getMessages() {
         return messages;
     }
-
+    /**
+     * Get student by name.
+     * @return Returns Student object with particular name.
+     * @param name - name of student.
+     */
     public Student getStudentByName(String name) {
         for (User user : users) {
             if (user instanceof Student) {
@@ -181,7 +237,11 @@ public class Database implements Serializable {
         }
         return null;
     }
-
+    /**
+     * Get student by id.
+     * @return Returns Student object with particular id.
+     * @param id - id of student.
+     */
     public Student getStudentById(String id) {
         for (User user : users) {
             if (user instanceof Student) {
@@ -193,19 +253,29 @@ public class Database implements Serializable {
         }
         return null;
     }
-
+    /**
+     * Encryption of file.
+     * @return boolean value of was file encrypted or not.
+     * @param data - data to encrypt.
+     */
     private static boolean encryptString(String data) {
         Base64.getEncoder().encodeToString((data.getBytes()));
         // System.out.println("Encryption is successful: " + encryptedData);
         return true;
     }
-
+    /**
+     * Decryption of file.
+     * @return boolean value of was file decrypted or not.
+     * @param data - data to decrypt.
+     */
     private static boolean decryptString(String data) {
         String decryptedData = new String(Base64.getDecoder().decode(data));
         // System.out.println("Decryption is successful: " + decryptedData);
         return true;
     }
-
+    /**
+     * Serialize all users.
+     */
     public static void saveUsers() {
         try (ObjectOutputStream oot = new ObjectOutputStream
                 (new FileOutputStream("users.dat"))) {
@@ -217,7 +287,9 @@ public class Database implements Serializable {
             System.err.println("users.dat: IOException");
         }
     }
-
+    /**
+     * Deserialize all users.
+     */
     @SuppressWarnings("unchecked")
     public static void loadUsers() {
         try {
@@ -237,7 +309,9 @@ public class Database implements Serializable {
 //            System.out.println(u);
 //        }
     }
-
+    /**
+     * Serialize all courses.
+     */
     public static void saveCourses() {
         try (ObjectOutputStream oot = new ObjectOutputStream
                 (new FileOutputStream("courses.dat"))) {
@@ -249,7 +323,9 @@ public class Database implements Serializable {
             System.err.println("courses.dat: IOException");
         }
     }
-
+    /**
+     * Deserialize all courses.
+     */
     @SuppressWarnings("unchecked")
     public static void loadCourses() {
         try {
@@ -266,14 +342,21 @@ public class Database implements Serializable {
             System.err.println("users.dat: IOException");
         }
     }
-
+    /**
+     * Get course by course code.
+     * @return Return course with particular code
+     * @param code - code of course
+     */
     public static Course getCourse(String code) {
         for (Course course: courses)
             if (course.getCode().equals(code))
                 return course;
         return null;
     }
-    
+    /**
+     * Delete course with particular course code.
+     * @param code - code of course
+     */
     public static void deleteCourse(String code) {
         for (Course course: courses)
             if (course.getCode().equals(code))
@@ -295,13 +378,22 @@ public class Database implements Serializable {
                 allMessages.add(message);
         return allMessages;
     }
-
+    /**
+     * Get user with particular username.
+     * @return Returns User object with partucular username.
+     * @param username - username of user
+     */
     public static User getUser(String username) {
         for (User user: users)
             if (user.getUsername().equals(username))
                 return user;
         return null;
     }
+    /**
+     * Get student with particular id.
+     * @return Returns Student object with partucular id.
+     * @param id - id of student
+     */
     public static Student getStudent(String id) {
     	for(User user: users) {
     		if (user instanceof Student) {
@@ -325,7 +417,11 @@ public class Database implements Serializable {
 //    	}
 //    	return null;
 //    }
-
+    /**
+     * Get teacher with particular id.
+     * @return Returns Teacher object with partucular id.
+     * @param id - id of teacher
+     */
     public static Teacher getTeacher(String id) {
         for(User user: users) {
             if (user instanceof Teacher) {
@@ -337,11 +433,16 @@ public class Database implements Serializable {
         }
         return null;
     }
-
+    /**
+     *Add news.
+     * @param news - News object to be added.
+     */
     public void addNews(News news) {
         Database.news.add(news);
     }
-
+    /**
+     * Serialize all nwes.
+     */
     private static void saveNews() {
         try (ObjectOutputStream oot = new ObjectOutputStream(new FileOutputStream("news.dat"))) {
             oot.writeObject(news);
@@ -351,7 +452,9 @@ public class Database implements Serializable {
             System.err.println("news.dat: IOException");
         }
     }
-
+    /**
+     * Deserialize all nwes.
+     */
     @SuppressWarnings("unchecked")
     private static void loadNews() {
         try {
--- Intranet/src/Manager.java
+++ Intranet/src/Manager.java
@@ -3,30 +3,30 @@ import java.util.Collections;
 
 
 /**
- * @generated
+ * Manager is employee who manages all users.
  */
 public class Manager extends Employee {
 
     /**
-     * @generated
+     * ArrayList of all courses.
      */
     private ArrayList<Course> courses;
 
     /**
-     * @generated
+     * ArrayList of all teachers.
      */
     private ArrayList <Teacher> teachers;
 
     /**
-     * @generated
+     * ArrayList of all students.
      */
     private ArrayList <Student> students;
-
-    private ArrayList <News> news;
+    
     /**
-     * @generated
+     * ArrayList of all news.
      */
-
+    private ArrayList <News> news;
+    
     public Manager(String id, String name, String surname, EmployeeTypes department) {
         super(id, name, surname, department);
     }
@@ -37,52 +37,64 @@ public class Manager extends Employee {
 
 
     /**
-     * @generated
+     * Returns all courses.
+     * @return Returns ArrayList of all courses.
      */
     public ArrayList <Course> getCourses() {
         return this.courses;
     }
 
     /**
-     * @generated
+     * Set all courses.
+     * @param courses - ArrayList of courses which need to be added
      */
     public void setCourses(ArrayList <Course> courses) {
         this.courses = courses;
     }
 
     /**
-     * @generated
+     * Returns all teachers.
+     * @return Returns ArrayList of all teachers.
      */
     public ArrayList <Teacher> getTeachers() {
         return this.teachers;
     }
 
     /**
-     * @generated
+     * Set all teachers.
+     * @param teachers - ArrayList of teachers which need to be added
      */
     public void setTeachers(ArrayList <Teacher> teachers) {
         this.teachers = teachers;
     }
 
     /**
-     * @generated
+     * Returns all students.
+     * @return Returns ArrayList of all students.
      */
     public ArrayList <Student> getStudents() {
         return this.students;
     }
 
     /**
-     * @generated
+     * Set all students.
+     * @param students - ArrayList of students which need to be added
      */
     public void setStudents(ArrayList <Student> students) {
         this.students = students;
     }
 
-
+    /**
+     * Returns all news.
+     * @return Returns ArrayList of all news.
+     */
     public ArrayList <News> getNews() {
         return this.news;
     }
-
+    /**
+     * Set all news.
+     * @param news - ArrayList of news which need to be added
+     */
     public void setNews(ArrayList<News> news) {
         this.news = news;
     }
@@ -94,7 +106,11 @@ public class Manager extends Employee {
 //    public createStatisticalReport() {
 //        //TODO
 //    }
-
+    /**
+     * Add course to courses.
+     * @param course - Course object which need to be added
+     * @return Return yes if course was successfully added and no if wasn't.
+     */
     public boolean addCourse(Course course) {
         if (course != null && course.getTeacher() != null) {
             for (Course courses: Database.courses)
@@ -106,65 +122,113 @@ public class Manager extends Employee {
         }
         return true;
     }
+    /**
+     * Delete course from courses.
+     * @param course - Course object which need to be deleted
+     */
     public void deleteCourse(Course course) {
 //        courses.remove(course);
         Database.courses.remove(course);
     }
+    /**
+     * Change limit of students of particular course.
+     * @param course - Course object which need to be changed
+     * @param number - new students limit for course
+     */
     public void changeLimitOfStudents(Course course, int number) {
         course.setLimitOfStudents(number);
     }
+    /**
+     * Change number of credits of particular course.
+     * @param course - Course object which need to be changed
+     * @param number - new number of credits for course
+     */
     public void changeNumberOfCredits(Course course, int number) {
         course.setCredits(number);
     }
+    /**
+     * Change registration status to open.
+     */
     public void openRegistration() {
         Database.registrationIsOpen = true;
     }
+    /**
+     * Change registration status to closed.
+     */
     public void closeRegistration() {
         Database.registrationIsOpen = false;
     }
-    
+    /**
+     *Get registration status.
+     */
     public boolean getRegistration() {
     	return Database.registrationIsOpen;
     }
-
+    
     public void extendRegistration() {
         if(!Database.registrationIsOpen) {
             Database.registrationIsOpen = true;
         }
     }
+    /**
+     * Add news.
+     * @param n - object of News class which need to be added.
+     */
     public void addNews(News n) {
         news.add(n);
     }
-
+    /**
+     *Update description of particular news.
+     * @param n - object of News class which need to be updated.
+     * @param description - new description of news
+     */
     public void updateNews(News n, String description) {
         n.setDescription(description);
     }
-
+    /**
+     * Delete news.
+     * @param n - object of News class which need to be deleted.
+     */
     public void deleteNews(News n) {
         news.remove(n);
     }
+    /**
+     * View teachers sorted by name.
+     * @return Return ArrayList of teachers sorted in alphabetic order.
+     */
     public ArrayList <Teacher> viewTeachersInAlphabeticOrder() {
         Collections.sort(teachers, new SortTeacherByName());
         return teachers;
     }
-
+    /**
+     * View courses of specific teacher.
+     * @param teacher - Teacher Object,we get his courses.
+     * @return Return ArrayList of courses of particular teacher.
+     */
     public ArrayList<Course> viewTeachersCourse(Teacher teacher) {
         return teacher.getCourses();
     }
-
+    /**
+     * View transcript of particular student.
+     * @param student - Student object,we see his transctipt.
+     */
     public void viewStudentsTranscript(Student student) {
         student.viewTranscript();
     }
-
+    /**
+     * View students sorted by name.
+     * @return Return ArrayList of students sorted in alphabetic order.
+     */
     public ArrayList <Student> viewStudentsInAlphabeticOrder() {
         Collections.sort(students, new SortStudentByName());
         return students;
     }
+    
     public ArrayList<Student>viewStudentsByGPA(Course course) {
         Collections.sort(students, new SortStudentByGpa(course));
         return students;
     }
-
+    
     @Override
     public String toString() {
         return "Manager{" +
